cc1042711d1b499768a78589272e2b175acfd129
kr-player <143769082+kr-player@users.noreply.github.com>
2023-09-14 00:02:34 +0900

    크롤링.py
    구글 뉴스의 뉴스 제목만을 모아서 나열한 것을 보여주고 싶었다.
    requests와 BeatifulSoup 라이브러리를 이용하여 크롤링을 진행하였다. 먼저 구글 뉴스의 검색 결과 링크를 가져오고 페이지의 내용을 가져온다. 내용을 잘 가져왔는지 확인을 해보고 BeatifulSoup를 사용하여 파싱을 한다. 그리고 특정 클래스의 이름을 가진 것을 찾고 제목 텍스트만을 추출하고 출력하도록 한다. 이렇게 해보았는 데 오류가 떠서 User-Agent 헤더를 통해 크롤링을 하기 위해 접근하려 했지만 실패하였다.

--- 크롤링.py
+++ 크롤링.py
@@ -1,15 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Google 검색 결과 페이지의 URL
 url = "https://www.google.com/search?q=%EB%89%B4%EC%8A%A4"
 
+# HTTP GET 요청을 보내서 페이지 내용을 가져옵니다.
 response = requests.get(url)
 
+# 요청이 성공했는지 확인합니다.
 if response.status_code == 200:
+    # BeautifulSoup를 사용하여 페이지의 HTML 내용을 파싱합니다.
     soup = BeautifulSoup(response.text, "html.parser")
 
+    # 특정 클래스 이름을 가진 요소를 모두 찾습니다.
     title_elements = soup.find_all("div", class_="n0jPhd ynAwRc MBeuO nDgy9d")
 
+    # 제목 텍스트를 추출하고 출력합니다.
     for title_element in title_elements:
         title_text = title_element.text
         print(title_text)
